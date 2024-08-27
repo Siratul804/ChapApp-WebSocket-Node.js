7783046d30d5909882a5e7022da3f5d4692f370d
Siratul Islam <islamsiratul@gmail.com>
2024-08-27 23:19:45 +0600

    chat-app

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { createServer } = require("node:http");
+const path = require("path");
+const app = express();
+const server = createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+//socket.io
+io.on("connection", (socket) => {
+  socket.on("user-message", (message) => {
+    io.emit("message", message);
+  });
+});
+
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res) => {
+  res.sendFile("/public/index.html");
+});
+
+server.listen(9000, () => {
+  console.log("server running at http://localhost:9000");
+});
